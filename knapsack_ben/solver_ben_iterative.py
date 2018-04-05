70ae0298471cf17cc2d1911beb2e714728b96fc3
Ben <ishmandoo@gmail.com>
2018-04-05 19:06:05 -0400

    added iterative option

--- knapsack_ben/solver_ben_iterative.py
+++ knapsack_ben/solver_ben_iterative.py
@@ -4,6 +4,7 @@
 from collections import namedtuple
 import sys
 import threading
+import queue
 
 sys.setrecursionlimit(5000)
 threading.stack_size(67108864) 
@@ -37,7 +38,36 @@ def solve_it(input_data):
     sorted_items = sorted(items, key = lambda item: item.density)
     #sorted_items = items
 
+    q = queue.LifoQueue()
 
+    def evaluate(args):
+        (i, taken, value, remaining) = args
+        global best_taken, best_value
+        if remaining < 0:
+            return -1
+        if i >= item_count:
+            if value > best_value:
+                best_value = value
+                best_taken = taken
+                #print("new best", value)
+            return value
+        item = sorted_items[i]
+        #if (value + optimisticValue(i, capacity - weight)) <= best_value:
+        if (value +  remaining * sorted_items[i].density) <= best_value:
+            #print("prune",(value + optimisticValueSimple(i, capacity - weight)) )
+            return -1
+
+        
+        q.put((i+1, taken + [item.index], value + item.value, remaining - item.weight))
+        q.put((i+1, taken, value, remaining))
+
+
+    q.put((0, [], 0, capacity))
+
+    while not q.empty():
+        evaluate(q.get())
+    
+    '''
     def dfs(i, taken, value, remaining):
         global best_taken, best_value
         #print(i,taken, value, weight)
@@ -50,17 +80,16 @@ def solve_it(input_data):
                 #print("new best", value)
             return value
 
-        item = sorted_items[i]
+        #if (value + optimisticValue(i, capacity - weight)) <= best_value:
         if (value +  remaining * sorted_items[i].density) <= best_value:
             #print("prune",(value + optimisticValueSimple(i, capacity - weight)) )
             return -1
 
         return max(
-            dfs(i+1, taken + [item.index], value + item.value, remaining - item.weight), 
+            dfs(i+1, taken + [i], value + sorted_items[i].value, remaining - sorted_items[i].weight), 
             dfs(i+1, taken, value, remaining)
             )
-
-    sol = dfs(0, [], 0, capacity)
+    '''
 
 
     taken = [0]*item_count
